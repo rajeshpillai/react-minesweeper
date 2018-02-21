491ccb2fbbb91b0b644dd83c0b33432f11eb03a1
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-02-21 10:53:58 +0530

    Fix css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
-import 'bulma';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
